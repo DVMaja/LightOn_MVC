31926ad6e0bb09e1b6cae31436e019dd533798c9
DVMaja <majadreilinger@gmail.com>
2023-10-10 08:49:23 +0200

    Controller példányosítások OK

--- controller/Controller.js
+++ controller/Controller.js
@@ -1,11 +1,19 @@
 import Modell from "../modell/Modell.js";
-import ElemView from "../view/ElemView.js";
+import Lampak from "../view/Lampak.js";
 
 class Controller {
-    
+    #vizszintesElemSzam;
+    #fuggolegesElemSzam;
 
     constructor() {
+        this.MODELL = new Modell();
         
+        this.#vizszintesElemSzam = 3;
+        this.#fuggolegesElemSzam = 3;
+
+        this.LAMPAK = new Lampak(this.#vizszintesElemSzam, this.#fuggolegesElemSzam, $(".jatekter")); 
+
+
     }
 }
 export default Controller;
